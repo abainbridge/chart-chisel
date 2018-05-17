85bc17b84cc079c83d9e2c4f1f1adffff7215851
Andrew Bainbridge <2005@deadfrog.co.uk>
2018-05-17 07:54:33 +0100

    Added command line parser. Added non-interactive "batch" mode. Made program save a BMP at exit.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 
 // Deadfrog headers
+#include "df_bmp.h"
 #include "df_font.h"
 #include "df_message_dialog.h"
 #include "df_time.h"
@@ -45,13 +46,36 @@ void FatalError(char const *fmt, ...)
 }
 
 
+void PrintUsageAndExit()
+{
+    puts("Usage: " APP_NAME " <mode> <input filename>");
+    puts("");
+    puts("Creates a message sequence chart as a PNG file.");
+    puts("");
+    puts("Where mode is either:");
+    puts("  batch       - Generates the output PNG and immediately exits.");
+    puts("  interactive - Generates the output PNG and displays it.");
+    exit(0);
+}
+
+
 int main(int argc, char *argv[])
 {
-    char const *filename = "../../message_sequence_charts/hello.msc";
-//    char const *filename = "../../message_sequence_charts/add_with_data.msc";
-    if (argc == 2) {
-        filename = argv[1];
+//     char *filename = "../../message_sequence_charts/hello.msc";
+//     g_interactiveMode = false;
+   char *filename = "../../message_sequence_charts/add_with_data.msc";
+    if (argc != 3) {
+        PrintUsageAndExit();
     }
+
+    if (stricmp(argv[1], "batch") == 0) {
+        g_interactiveMode = false;
+    }
+    else if (stricmp(argv[1], "interactive") != 0) {
+        printf("Unrecognized mode '%s'\n", argv[1]);
+        return -1;
+    }
+    filename = argv[2];
 
     g_defaultFont = FontCreate("Arial", 10, 7);
     g_antialiasedDraw = new AntialiasedDraw;
@@ -59,21 +83,37 @@ int main(int argc, char *argv[])
     MessageSequenceChart msc;
     msc.Load(filename);
 
-    // Setup the window
-    int width, height;
-    GetDesktopRes(&width, &height);
-    CreateWin(1200, height - 100, WT_WINDOWED, APP_NAME);
-    BitmapClear(g_window->bmp, g_colourWhite);
+    DfBitmap *bmp = NULL;
+    if (g_interactiveMode) {
+        // Setup the window
+        int width, height;
+        GetDesktopRes(&width, &height);
+        CreateWin(1200, height - 100, WT_WINDOWED, APP_NAME);
+        bmp = g_window->bmp;
+        BitmapClear(bmp, g_colourWhite);
 
-    // Continue to display the window until the user presses escape or clicks the close icon
-    double lastTime = GetRealTime();
-    while (!g_window->windowClosed && !g_input.keys[KEY_ESC])
-    {
-        InputManagerAdvance();
-        BitmapClear(g_window->bmp, g_colourWhite);
-        msc.Render(g_window->bmp);
-        UpdateWin();
-        SleepMillisec(4);
+        // Continue to display the window until the user presses escape or clicks the close icon
+        double lastTime = GetRealTime();
+        while (!g_window->windowClosed && !g_input.keys[KEY_ESC])
+        {
+            InputManagerAdvance();
+            BitmapClear(bmp, g_colourWhite);
+            msc.Render(bmp);
+            UpdateWin();
+            SleepMillisec(4);
+        }
+    }
+    else {
+        bmp = BitmapCreate(1300, 1600);
+        BitmapClear(bmp, g_colourWhite);
+        msc.Render(bmp);
+    }
+
+    char *outputFilename = strdup(filename);
+    int len = strlen(outputFilename);
+    memcpy(outputFilename + len - 4, ".bmp", 4);
+    if (!SaveBmp(bmp, outputFilename)) {
+        FatalError("Couldn't write output to '%s'", outputFilename);
     }
 
     return 0;
